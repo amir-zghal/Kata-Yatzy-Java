0f3367ae6bd7702e63d0577399496bcab47dad07
Amir Zghal <amir.zghal@gmail.com>
2022-07-31 20:45:52 +0200

    Add javaDoc

--- src/main/java/com/amir/yatzy/DicesScroll.java
+++ src/main/java/com/amir/yatzy/DicesScroll.java
@@ -7,6 +7,13 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+/**
+ * DiceScroll class represente la manipulation d'une lancé de dés
+ *
+ * @author Amir Zghal
+ *
+ * @version 1.0
+ */
 public class DicesScroll {
 
     private static final List<Integer> SMALL_STRAIGTH = Arrays.asList(1, 2, 3, 4, 5);
@@ -14,40 +21,88 @@ public class DicesScroll {
 
     private final List<Integer> dicesResults;
 
+    /**
+     * Instantiation d'un nouveau Dices scroll.
+     *
+     * @param dice1 le dés 1
+     * @param dice2 le dés 2
+     * @param dice3 le dés 3
+     * @param dice4 le dés 4
+     * @param dice5 le dés 5
+     */
     public DicesScroll(int dice1, int dice2, int dice3, int dice4, int dice5) {
         this.dicesResults = Arrays.asList(dice1, dice2, dice3, dice4, dice5);
     }
 
+    /**
+     * la somme de tous les dés
+     *
+     * @return un int qui represente la somme de tous les dés
+     */
     public int sum() {
         return dicesResults.stream()
             .reduce(0, Integer::sum);
     }
 
+    /**
+     * Compter les dés avec le nombre d'occurrences
+     *
+     * @return Un map de dés avec le nombre d'occurrences
+     */
     private Map<Integer, Integer> countDices() {
         return dicesResults.stream()
             .collect(Collectors.groupingBy(d -> d, Collectors.reducing(0, e -> 1, Integer::sum)));
     }
 
+    /**
+     * Compter les occurrences de dés
+     *
+     * @param dice la valeur de dés à compter
+     * @return un int qui represente le nombre d'occurrences pour la valeur donnée
+     */
     public int countDice(int dice) {
         return countDices().getOrDefault(dice, 0);
     }
 
+    /**
+     * Compter les occurrences de dés multipliées par les dés
+     *
+     * @param dice la valeur de dés à compter
+     * @return un int qui represente le nombre d'occurrences pour la valeur donnée multiplié par la valeur du dés
+     */
     public int countDiceMultiByDice(int dice) {
         return countDice(dice) * dice;
     }
 
+    /**
+     * Obtenir un stream des dés avec un nombre supperieur à la valeur donnée
+     *
+     * @param count le nombre count
+     * @return un stream de Integer
+     */
     private Stream<Integer> getDiceValueWithCountGreaterThan(int count) {
         return countDices().entrySet().stream()
             .filter(entry -> entry.getValue() >= count)
             .map(Map.Entry::getKey);
     }
 
+    /**
+     * Trouver les paires de valeurs de dés
+     *
+     * @return une list de Integer qui represente liste de paires triées par valeur descendante
+     */
     public List<Integer> findPairsDesc() {
         return getDiceValueWithCountGreaterThan(2)
             .sorted(Comparator.reverseOrder())
             .collect(Collectors.toList());
     }
 
+    /**
+     * Calculer la paire par limite
+     *
+     * @param limit le nombre limite
+     * @return un int qui représente la somme du couple limité par un limitateur et multipliée par deux
+     */
     public int calculatePair(int limit) {
         List<Integer> pairs = findPairsDesc();
         if (pairs.isEmpty()) {
@@ -58,24 +113,50 @@ public class DicesScroll {
             .reduce(0, Integer::sum);
     }
 
+    /**
+     * Obtenir le premier dés dont le nombre est supérieur au nombre fourni
+     *
+     * @param count le nombre count
+     * @return un int qui représente le dés dont le nombre est supérieur au nombre fourni, renvoie 0 s'il n'y a pas de correspondance
+     */
     public int getFirstDiceWithCountGreaterThan(int count) {
         return getDiceValueWithCountGreaterThan(count)
             .findFirst()
             .orElse(0);
     }
 
+    /**
+     * Trier les dés par ordre croissant
+     *
+     * @return une list de Integer avec un ordre croissant
+     **/
     private List<Integer> orderAsc() {
         return dicesResults.stream().sorted().collect(Collectors.toList());
     }
 
+    /**
+     * Deternimer si les dés sont small straight
+     *
+     * @return un boolean qui represente true si small straight, false sinon
+     */
     public boolean isSmallStraight() {
         return orderAsc().equals(SMALL_STRAIGTH);
     }
 
+    /**
+     * Deternimer si les dés sont large straight
+     *
+     * @return un boolean qui represente true si large straight, false sinon
+     */
     public boolean isLargeStraight() {
         return orderAsc().equals(LARGE_STRAIGHT);
     }
 
+    /**
+     * Determiner si les dés sont yatzy.
+     *
+     * @return un boolean qui represente true si yatzy, false sinon
+     */
     public boolean isYatzy() {
         return countDices()
             .values()
@@ -83,6 +164,11 @@ public class DicesScroll {
             .anyMatch(count -> count == 5);
     }
 
+    /**
+     * Determiner si les dés sont full house
+     *
+     * @return un boolean qui represente true si full house, false sinon
+     */
     public boolean isFullHouse() {
         int threeOfAKindValue = getFirstDiceWithCountGreaterThan(3);
         int pairValue = findPairsDesc().stream().findFirst().orElse(0);
--- src/main/java/com/amir/yatzy/Yatzy.java
+++ src/main/java/com/amir/yatzy/Yatzy.java
@@ -1,81 +1,177 @@
 package com.amir.yatzy;
 
-
+/**
+ * Yatzy class represente le calcule Yatzy pour une lancé de dés
+ *
+ * @author Amir Zghal
+ *
+ * @version 1.0
+ */
 public class Yatzy {
 
     public Yatzy() {
     }
 
+    /**
+     * Obtenir la chance
+     *
+     * @param dicesScroll le dices scroll
+     * @return un int qui représente le resultat
+     */
     public static int chance(DicesScroll dicesScroll)
     {
         return dicesScroll.sum();
     }
 
+    /**
+     * Obtenir le yatzy
+     *
+     * @param dicesScroll le dices scroll
+     * @return un int qui représente le resultat
+     */
     public static int yatzy(DicesScroll dicesScroll)
     {
         return dicesScroll.isYatzy() ? 50 : 0;
     }
 
+    /**
+     * Obtenir les uns
+     *
+     * @param dicesScroll le dices scroll
+     * @return un int qui représente le resultat
+     */
     public static int ones(DicesScroll dicesScroll)
     {
         return dicesScroll.countDiceMultiByDice(1);
     }
 
+    /**
+     * Obtenir les deux
+     *
+     * @param dicesScroll le dices scroll
+     * @return un int qui représente le resultat
+     */
     public static int twos(DicesScroll dicesScroll)
     {
         return dicesScroll.countDiceMultiByDice(2);
     }
 
+    /**
+     * Obtenir les trois
+     *
+     * @param dicesScroll le dices scroll
+     * @return un int qui représente le resultat
+     */
     public static int threes(DicesScroll dicesScroll)
     {
         return dicesScroll.countDiceMultiByDice(3);
     }
 
+    /**
+     * Obtenir les quatres
+     *
+     * @param dicesScroll le dices scroll
+     * @return un int qui représente le resultat
+     */
     public static int fours(DicesScroll dicesScroll)
     {
         return dicesScroll.countDiceMultiByDice(4);
     }
 
+    /**
+     * Obtenir les cinqs
+     *
+     * @param dicesScroll le dices scroll
+     * @return un int qui représente le resultat
+     */
     public static int fives(DicesScroll dicesScroll)
     {
         return dicesScroll.countDiceMultiByDice(5);
     }
 
+    /**
+     * Obtenir les six
+     *
+     * @param dicesScroll le dices scroll
+     * @return un int qui représente le resultat
+     */
     public static int sixes(DicesScroll dicesScroll)
     {
         return dicesScroll.countDiceMultiByDice(6);
     }
 
+    /**
+     * Obtenir le une paire
+     *
+     * @param dicesScroll le dices scroll
+     * @return un int qui représente le resultat
+     */
     public static int onePair(DicesScroll dicesScroll)
     {
         return dicesScroll.calculatePair(1);
     }
 
+    /**
+     * Obtenir le deux paire
+     *
+     * @param dicesScroll le dices scroll
+     * @return un int qui représente le resultat
+     */
     public static int twoPair(DicesScroll dicesScroll)
     {
         return dicesScroll.calculatePair(2);
     }
 
-    public static int fourOfAKind(DicesScroll dicesScroll)
-    {
-        return dicesScroll.getFirstDiceWithCountGreaterThan(3) * 4;
-    }
-
+    /**
+     * Obtenir le brelan
+     *
+     * @param dicesScroll le dices scroll
+     * @return un int qui représente le resultat
+     */
     public static int threeOfAKind(DicesScroll dicesScroll)
     {
         return dicesScroll.getFirstDiceWithCountGreaterThan(3) * 3;
     }
 
+    /**
+     * Obtenir le carré
+     *
+     * @param dicesScroll le dices scroll
+     * @return un int qui représente le resultat
+     */
+    public static int fourOfAKind(DicesScroll dicesScroll)
+    {
+        return dicesScroll.getFirstDiceWithCountGreaterThan(3) * 4;
+    }
+
+    /**
+     * Obtenir le small straight
+     *
+     * @param dicesScroll le dices scroll
+     * @return un int qui représente le resultat
+     */
     public static int smallStraight(DicesScroll dicesScroll)
     {
         return dicesScroll.isSmallStraight() ? 15 : 0;
     }
 
+    /**
+     * Obtenir le large straight
+     *
+     * @param dicesScroll le dices scroll
+     * @return un int qui représente le resultat
+     */
     public static int largeStraight(DicesScroll dicesScroll)
     {
         return dicesScroll.isLargeStraight() ? 20 : 0;
     }
 
+    /**
+     * Obtenir le full house
+     *
+     * @param dicesScroll le dices scroll
+     * @return un int qui représente le resultat
+     */
     public static int fullHouse(DicesScroll dicesScroll)
     {
         return dicesScroll.isFullHouse() ? dicesScroll.sum() : 0;
